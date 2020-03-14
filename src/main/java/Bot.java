fde0258dd7ed19a47541748229fd04d6830784c5
Lars <Lars.heublein@web.de>
2020-03-14 19:56:05 +0100

    POC for fancy messages,
    Adds and removes Aatrox emoji from local file with each start.
    .help command shows fancy WIP messages.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,8 +2,17 @@ import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.util.Image;
+import discord4j.core.spec.EmbedCreateSpec;
+import reactor.core.publisher.Mono;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.*;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Bot {
 
@@ -56,11 +65,55 @@ public class Bot {
         return 0;
     }
 
-    private Integer help(Message message) {
-        message.getChannel().block().createMessage("You can message this bot directly.\n" +
+    private Integer help(Message m) {
+        var messageChannel = m.getChannel().block();
+        Mono<Message> message = messageChannel.createMessage(messageSpec -> {
+            messageSpec.setContent("Content not in an embed!");
+            // You can see in this example even with simple singular property defining specs the syntax is concise
+            messageSpec.setEmbed(embedSpec -> {
+                embedSpec.setDescription("Description is in an embed!");
+                embedSpec.setColor(Color.PINK);
+            });
+        });
+        message.block();
+        messageChannel.createMessage("You can message this bot directly.\n" +
                 "`.help` will display this very help message.\n" +
                 "`.matches` will list matches per day for a given user.\n" +
                 "`.clash` will return suitable bans against a list of players.").block();
+        Mono<Message> message2 = messageChannel.createMessage(messageSpec -> {
+            // You can see in this example even with simple singular property defining specs the syntax is concise
+            messageSpec.setEmbed(embedSpec -> {
+                System.out.println("starting");
+                embedSpec.setColor(Color.PINK).setImage("https://i.imgur.com/wSTFkRM.png");
+                embedSpec.setDescription("I'm testing embeds!");
+                embedSpec.setTitle("Kuchen");
+                embedSpec.addField("Cat", "HI :wave: \n miau", true);
+                embedSpec.addField("Dog", "HO :wave:", true);
+                embedSpec.addField("Cat", "miau", false)
+                        .setUrl("https://i.imgur.com/wSTFkRM.png");
+                embedSpec.setDescription("some other");
+            });
+        });
+        messageChannel.createMessage(messageSpec -> {
+            // You can see in this example even with simple singular property defining specs the syntax is concise
+            messageSpec.setEmbed(embedSpec -> {
+                System.out.println("starting");
+                embedSpec.setColor(Color.PINK).setImage("https://i.imgur.com/wSTFkRM.png");
+                embedSpec.setDescription("I'm testing embeds!");
+                embedSpec.setTitle("Kuchen");
+                embedSpec.addField("Elephant", "HI :wave: \n miau", true);
+                embedSpec.addField("Dog", "HO :wave:", true);
+                embedSpec.addField("Cat", "miau", false)
+                        .setUrl("https://i.imgur.com/wSTFkRM.png");
+                embedSpec.setDescription("some other");
+            });
+            messageSpec.setContent("I like to eat cake!");
+            messageSpec.setTts(true);
+
+        }).block();
+        message2.block();
+
+        System.out.println("done");
         return 0;
     }
 
@@ -77,6 +130,22 @@ public class Bot {
     public static void main(String[] args) {
         var s = new Bot(args[0], args[1]);
         s.getCommands(s.client);
+
+        s.client.getGuilds().blockFirst().createEmoji(spec -> {
+            File fi = new File("Aatrox.png");
+            try {
+                byte[] fileContent = Files.readAllBytes(fi.toPath());
+                spec.setImage(Image.ofRaw(fileContent, Image.Format.PNG));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            spec.setName("myaatrox");
+
+        }).block();
+        s.client.getGuilds().blockFirst().getEmojis().filter(ge -> ge.getName().equals("myaatrox")).blockFirst().delete().block();
+
+
+        System.out.println("emoji");
         s.client.login().block();
     }
 
