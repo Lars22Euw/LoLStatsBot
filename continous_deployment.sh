f11d82ab751beac24848575982d4813ce39a0299
ThomasRot <pi@example.com>
2020-04-18 19:00:43 +0200

    fix exposed secrets

--- continous_deployment.sh
+++ continous_deployment.sh
@@ -2,8 +2,10 @@ set -ex
 echo Starting to listen
 while true; do
 kill $(ps auwx | grep java | grep -v grep | awk '{print $1}') || true
+echo $(cat discordapi.txt)
+echo $(cat riotapi.txt)
 mvn install
-mvn exec:java -Dexec.mainClass=Bot -Dexec.args="RGAPI-4e62aa33-98eb-44e6-914e-5581379573fe NjQ0OTgzMTY4NzUxMTA4MTEy.Xla_ng.eNOyAL0mNAYrGxQpHOwNzlf0zk0"&
+mvn exec:java -Dexec.mainClass=Bot -Dexec.args="$(cat riotapi.txt) $(cat discordapi.txt)"&
 while git pull | grep Already; do
 sleep 30
 done
